984f15421909d6865bbe00d1ca95d8c0e0cfdba3
Michael Lee <michaelleemichaellee408@gmail.com>
2022-02-18 19:16:32 +0100

    Plugin initialization

---
+++ wp-checklist.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: WP-Checklist
+Plugin URI: http://wordpress.org
+Description: A Simple Plugin for creating a todo list.
+Version: 1.0
+Author: Michael Lee
+Author URI: http://wordpress.org
+License: GPLv2
+*/
+ 
+defined( 'ABSPATH' ) || exit;
+?>
