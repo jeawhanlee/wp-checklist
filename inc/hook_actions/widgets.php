9a8789016478689a0be42d3a20568fe970f34a7f
Michael Lee <michaelleemichaellee408@gmail.com>
2022-02-21 23:22:30 +0100

    created todo modifiers

--- inc/hook_actions/widgets.php
+++ inc/hook_actions/widgets.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 
 function wp_ml_dashboard_widgets() {
  
@@ -15,13 +16,103 @@ function wp_ml_dashboard_widgets() {
 function wp_ml_list_display() {
     $tasks = get_option( 'wp_ml_todo' );
 
+    // display todos
+    display_todo( $tasks );
+}
+
+function wp_ml_list_setup() {
+    $tasks = get_option( 'wp_ml_todo' );
+
+    // add todo
+    add_todo( $tasks );
+
+    // remove todo
+    remove_todo( $tasks );
+
+    // complete todo
+    complete_todo( $tasks );
+    
+    display_todo( $tasks, true );
+}
+
+function add_todo( $tasks ) {
+
+    if( isset( $_POST['wp_ml_todo'] ) ){
+
+        $todo = $_POST['wp_ml_todo'];
+        $date_added = date("Y-m-d");
+        $id = date('ymdhis');
+
+        $task[$id] = [ $todo, $date_added, "pending" ];
+
+        if( !empty( $tasks ) ){
+            $tasks = json_decode( $tasks, true );
+
+            foreach( $tasks as $task_id => $meta ) {
+                $task[$task_id] = $meta;
+            }
+        }
+        
+        $task = json_encode( $task );
+
+        update_option( 'wp_ml_todo', $task );
+    }
+}
+
+function remove_todo( $tasks ){
+
+    if( isset( $_GET['remove'] ) ){
+
+        $task_id = $_GET['task_id'];
+
+        $tasks = json_decode( $tasks, true );
+        unset( $tasks[$task_id] );
+
+        $tasks = !empty( $tasks ) ? json_encode( $tasks ) : '';
+        update_option( 'wp_ml_todo', $tasks );
+
+        wp_safe_redirect( admin_url() );
+        exit;
+    }
+}
+
+function complete_todo( $tasks ){
+    if( isset( $_GET['complete'] ) ){
+
+        $task_id = $_GET['task_id'];
+
+        $tasks = json_decode( $tasks, true );
+        $tasks[$task_id][2] = 'completed';
+
+        $tasks = json_encode( $tasks );
+        update_option( 'wp_ml_todo', $tasks );
+
+        wp_safe_redirect( admin_url() );
+        exit;
+    }
+}
+
+function display_todo( string $tasks, $config = false ){
+
     if( !empty( $tasks ) ) {
-        $tasks = explode(',', $tasks);
+        $tasks = json_decode( $tasks, true );
         
         echo '<ul>';
-        foreach( $tasks as $task ) {
+        foreach( $tasks as $task_id => $meta ) {
+            $task_entity = explode('-', $task);
+            $date = date_create( $meta[1] );
         ?>  
-            <li>- <?php echo ucwords( $task ) ?></li>
+            <li>
+                <strong <?php echo $meta[2] == 'completed' ? 'style="text-decoration:line-through"' : '' ?> ><?php echo ucwords( $meta[0] ) ?></strong>
+                <br />
+                <small><?php echo date_format( $date, 'd/m/Y' ) ?></small> | 
+                <?php if( $config ): ?> 
+                    <a href="<?php echo admin_url() ?>?edit=dashboard_todolist&complete=true&task_id=<?php echo $task_id ?>#dashboard_todolist" style="color:green">Mark as complete</a> | 
+                    <a href="<?php echo admin_url() ?>?edit=dashboard_todolist&remove=true&task_id=<?php echo $task_id ?>#dashboard_todolist" style="color:red">Remove</a>
+                <?php else: ?>
+                    <small><?php echo ucfirst($meta[2]) ?></small>
+                <?php endif ?>
+            </li>
         <?php
         }
         echo '</ul>';
@@ -29,21 +120,9 @@ function wp_ml_list_display() {
     else {
         echo '<p>No task added</p>';
     }
-}
 
-function wp_ml_list_setup() {
-    $option = get_option( 'wp_ml_todo' );
-
-    if( isset( $_POST['wp_ml_todo'] ) ){
-        $todo = $_POST['wp_ml_todo'];
-
-        if( !empty( $option ) ){
-            $todo = "$option, $todo";
-        }
-
-        update_option( 'wp_ml_todo', $todo );
+    // display form if config
+    if( $config ) {
+        echo '<input type="text" name="wp_ml_todo" placeholder="Add a new task" style="margin-bottom:10px;width:100%" />';
     }
-?>
-    <input type="text" name="wp_ml_todo" placeholder="Add a Task" style="margin-bottom:10px;" />
-<?php
 }
