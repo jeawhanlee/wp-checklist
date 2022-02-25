5a2b06333572bb227db46c21dfdf4bef9bf61f88
Michael Lee <michaelleemichaellee408@gmail.com>
2022-02-25 13:50:03 +0100

    phpcbf fix

--- inc/hook_actions/actions.php
+++ inc/hook_actions/actions.php
@@ -5,83 +5,83 @@ namespace WP_Checklist\hook_actions;
 defined( 'ABSPATH' ) || exit;
 
 class Actions {
-    
-    /**
-     * hold task array
-     *
-     * @var array
-     */
+
+	/**
+	 * hold task array
+	 *
+	 * @var array
+	 */
 	protected $tasks = array();
 
-    /**
-     * get all task on inst
-     */
+	/**
+	 * get all task on inst
+	 */
 	public function __construct() {
 
-		$this->tasks = get_option( 'wp_ml_todo' ) ?: [];
+		$this->tasks = get_option( 'wp_ml_todo' ) ?: array();
 
 	}
 
-    /**
-     * Create new todo
-     *
-     * @return void
-     */
+	/**
+	 * Create new todo
+	 *
+	 * @return void
+	 */
 	protected function add_todo() {
 
 		if ( ! isset( $_POST['wp_ml_todo'] ) ) {
-            return;
-        }
+			return;
+		}
 
-        $todo       = $_POST['wp_ml_todo'];
-        $date_added = date( 'Y-m-d' );
-        $id         = date( 'ymdhis' );
+		$todo       = $_POST['wp_ml_todo'];
+		$date_added = date( 'Y-m-d' );
+		$id         = date( 'ymdhis' );
 
-        $this->tasks[ $id ] = array( $todo, $date_added, 'pending' );
+		$this->tasks[ $id ] = array( $todo, $date_added, 'pending' );
 
-        update_option( 'wp_ml_todo', $this->tasks );
+		update_option( 'wp_ml_todo', $this->tasks );
 	}
 
-    /**
-     * remove todo from array
-     *
-     * @return void
-     */
+	/**
+	 * remove todo from array
+	 *
+	 * @return void
+	 */
 	protected function remove_todo() {
 
 		if ( ! isset( $_GET['remove'] ) ) {
-            return;
-        }
+			return;
+		}
 
-        $task_id = $_GET['task_id'];
+		$task_id = $_GET['task_id'];
 
-        unset( $this->tasks[ $task_id ] );
+		unset( $this->tasks[ $task_id ] );
 
-        update_option( 'wp_ml_todo', ! empty( $this->tasks ) ? $this->tasks : '' );
+		update_option( 'wp_ml_todo', ! empty( $this->tasks ) ? $this->tasks : '' );
 
-        wp_safe_redirect( admin_url() );
-        exit;
+		wp_safe_redirect( admin_url() );
+		exit;
 	}
 
-    /**
-     * set todo as complete
-     *
-     * @return void
-     */
+	/**
+	 * set todo as complete
+	 *
+	 * @return void
+	 */
 	protected function complete_todo() {
 
-        if ( ! isset( $_GET['complete'] ) ) {
-            return;
-        }
+		if ( ! isset( $_GET['complete'] ) ) {
+			return;
+		}
 
-        $task_id = $_GET['task_id'];
+		$task_id = $_GET['task_id'];
 
-        $this->tasks[ $task_id ][2] = 'completed';
+		$this->tasks[ $task_id ][2] = 'completed';
 
-        update_option( 'wp_ml_todo', $this->tasks );
+		update_option( 'wp_ml_todo', $this->tasks );
 
-        wp_safe_redirect( admin_url() );
-        exit;
+		wp_safe_redirect( admin_url() );
+		exit;
 	}
 }
 
--- inc/hook_actions/widgets.php
+++ inc/hook_actions/widgets.php
@@ -6,11 +6,11 @@ defined( 'ABSPATH' ) || exit;
 
 class Widgets extends \WP_Checklist\hook_actions\Actions {
 
-    /**
-     * display widget on admin dashboard
-     *
-     * @return void
-     */
+	/**
+	 * display widget on admin dashboard
+	 *
+	 * @return void
+	 */
 	public function wp_ml_dashboard_widgets() {
 
 		// create a custom dashboard widget
@@ -22,11 +22,11 @@ class Widgets extends \WP_Checklist\hook_actions\Actions {
 		);
 	}
 
-    /**
-     * display todo list on default
-     *
-     * @return void
-     */
+	/**
+	 * display todo list on default
+	 *
+	 * @return void
+	 */
 	public function wp_ml_list_display() {
 
 		// display todos
@@ -34,12 +34,12 @@ class Widgets extends \WP_Checklist\hook_actions\Actions {
 
 	}
 
-    /**
-     * controller call back for performing
-     * todo actions (add, remove, mark as complete)
-     *
-     * @return void
-     */
+	/**
+	 * controller call back for performing
+	 * todo actions (add, remove, mark as complete)
+	 *
+	 * @return void
+	 */
 	public function wp_ml_list_setup() {
 
 		// add todo
@@ -55,14 +55,14 @@ class Widgets extends \WP_Checklist\hook_actions\Actions {
 		return $this->display_todo( $this->tasks, true );
 	}
 
-    /**
-     * require html view for displaying todos
-     * with check for config mode
-     *
-     * @param array $tasks
-     * @param boolean $config
-     * @return void
-     */
+	/**
+	 * require html view for displaying todos
+	 * with check for config mode
+	 *
+	 * @param array   $tasks
+	 * @param boolean $config
+	 * @return void
+	 */
 	public function display_todo( array $tasks, $config = false ) {
 
 		include_once CONFIG['app_path'] . 'view/todos.php';
@@ -71,13 +71,12 @@ class Widgets extends \WP_Checklist\hook_actions\Actions {
 
 	}
 
-    /**
-     * execute plugin
-     *
-     *
-     * @return void
-     */
-    public function wp_ml_execute() {
+	/**
+	 * execute plugin
+	 *
+	 * @return void
+	 */
+	public function wp_ml_execute() {
 		add_action( 'wp_dashboard_setup', array( $this, 'wp_ml_dashboard_widgets' ) );
 	}
 }
